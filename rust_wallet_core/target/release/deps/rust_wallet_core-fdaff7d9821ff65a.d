a96b883011f9e9ae494b9bb02f921db1b200aea5
Ibrahim <ibirfkazi@gmail.com>
2025-06-16 12:19:25 +0530

    do rust setup

---
+++ rust_wallet_core/target/release/deps/rust_wallet_core-fdaff7d9821ff65a.d
@@ -0,0 +1,7 @@
+C:\Users\Ibrahim\Desktop\blockchain\bio-wallet\rust_wallet_core\target\release\deps\librust_wallet_core-fdaff7d9821ff65a.rmeta: src\lib.rs
+
+C:\Users\Ibrahim\Desktop\blockchain\bio-wallet\rust_wallet_core\target\release\deps\librust_wallet_core-fdaff7d9821ff65a.rlib: src\lib.rs
+
+C:\Users\Ibrahim\Desktop\blockchain\bio-wallet\rust_wallet_core\target\release\deps\rust_wallet_core-fdaff7d9821ff65a.d: src\lib.rs
+
+src\lib.rs:
---
+++ rust_wallet_core/target/release/librust_wallet_core.d
@@ -0,0 +1,1 @@
+C:\Users\Ibrahim\Desktop\blockchain\bio-wallet\rust_wallet_core\target\release\librust_wallet_core.rlib: C:\Users\Ibrahim\Desktop\blockchain\bio-wallet\rust_wallet_core\src\lib.rs
